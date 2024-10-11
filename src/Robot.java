f319b8dc2d9ccd57089aa8cb5c4835ea911e2b1d
David Petri <david300900@gmail.com>
2024-10-11 21:29:09 +0200

    Turn robot into abstract class and manage visibility of attributes

--- src/Robot.java
+++ src/Robot.java
@@ -1,7 +1,14 @@
-public interface Robot {
-    public Case getPosition();
-    public void setPosition(Case c);
-    public double getVitesse(NatureTerrain nature);
-    public void deverserEau(int vol);
-    public void remplirReservoir();
+public abstract class Robot {
+    private Case position;
+
+    public Robot(Case position)
+    {
+        this.position = position;
+    }
+
+    public Case getPosition() { System.out.println("Mon position est n'importe ou ") };
+    public void setPosition(Case c) { System.out.println("Mon nouveau position est quoi?"); }
+    public double getVitesse(NatureTerrain nature) { Syste.out.println("Je rpule à 100km/h "); }
+    public void deverserEau(int vol) { System.out.println("Je deverse d'eau"); }
+    public void remplirReservoir() { System.out.println("Remplir Reservoir"); }
 }
